1a02640082053aaf6f92f7d9654a8d37ca953721
Tim Sagona <tsagona@1904labs.com>
2020-03-09 11:00:05 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder.appName("SparkPythonStarter").getOrCreate()
+
+lines = spark.read.option("inferSchema", True).option("header", True).csv("file:///tmp/people.txt")
+lines.printSchema()
+
+doubledAges = lines.select(col("name"), col("age")*2, col("dept"))
+doubledAges.foreach(print)
+
+averageAgeByDept = lines.groupBy(col("dept")).avg("age")
+averageAgeByDept.foreach(print)
+
+
